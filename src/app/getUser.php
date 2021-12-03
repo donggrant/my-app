a78d7c7d279da7c8251532d0f63aed78c0d7244d
Grant Dong <gdd7jh@virginia.edu>
2021-12-02 23:18:40 -0500

    Added files

---
+++ src/app/getUser.php
@@ -0,0 +1,71 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT");
+
+/******************************/
+// [Spring 2021] connecting to DB on CS server
+$username = 'root';
+$password = '';
+$host = 'localhost';
+$dbname = 'pet_pals';
+/******************************/
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+$db = "";
+
+/** connect to the database **/
+try 
+{
+   $db = new PDO($dsn, $username, $password);   
+}
+catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+{
+   // Call a method from any object, 
+   // use the object's name followed by -> and then method's name
+   // All exception objects provide a getMessage() method that returns the error message 
+   $error_message = $e->getMessage();        
+   echo "<p>An error occurred while connecting to the database: $error_message </p>";
+}
+catch (Exception $e)       // handle any type of exception
+{
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
+}
+
+
+/** SETUP **/
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli($host, $username, $password, $dbname);
+// $db = new mysql("localhost", "root", "", "dbname"); // XAMPP Settings
+$user = null;
+
+session_start();
+// Example using URL rewriting: we add the user information
+// directly to the URI with a query string (GET parameters)
+
+$q = $_GET['q'];
+
+$sql="SELECT * FROM users WHERE name like '%$q%'";
+$result = mysqli_query($mysqli,$sql);
+
+echo "<table>
+<tr>
+<th>Name</th>
+<th>Email</th>
+<th>User Type</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['email'] . "</td>";
+  echo "<td>" . $row['type'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($mysqli);
+
+?>
