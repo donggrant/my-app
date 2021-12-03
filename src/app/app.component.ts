0f5270deaa35ead435502fadf5ee189c3e0f70d0
hpv8hf <hpv8hf@virginia.edu>
2021-12-03 11:46:00 -0500

    fixed angular component functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Pet Pals';
   author = "Grant Dong and Hunter Vaccaro";
+  name  = ""; 
+
+  showUsers(a: string) {   
+    var xmlhttp = new XMLHttpRequest();
+    this.name = a;
+    console.log(a);
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) { 
+        window.name = this.responseText; 
+      }
+    };  
+    console.log(this.name);
+    xmlhttp.open("GET","getUser.php?q="+a,true);
+    xmlhttp.send(); 
+  }
 }
